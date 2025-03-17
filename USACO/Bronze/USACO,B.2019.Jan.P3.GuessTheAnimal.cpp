61d891ad5c1410b30035c951abb3bd511db36f64
gitpanda2013 <om_chatterjee@outlook.com>
2025-03-17 13:13:06 -0400

    Manual Upload

---
+++ USACO/Bronze/USACO,B.2019.Jan.P3.GuessTheAnimal.cpp
@@ -0,0 +1,49 @@
+// USACO 2019 January Contest, Bronze
+// Problem 3. Guess the Animal
+// USACO Question : https://usaco.org/index.php?page=viewproblem2&cpid=893
+// USACO Guide Link : https://usaco.guide/groups/d7eYGfddXq3m2trXG2xt/post/HUiqbm1YRSctsEgyrY4e
+// Google Slide Explanation : https://docs.google.com/presentation/d/1gLWrnoPlCJluiTZKSg0-DQ3ay24_snq7Vi7JXkyrlAY/edit#slide=id.p
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int num_entry = 0;
+    cin >> num_entry;
+
+    vector<vector<string>> animalQuestions(num_entry);
+
+    for (int i=0;i<num_entry;i++) {
+        string animalName; 
+        int numQ;
+        cin >> animalName; 
+        cin >> numQ;
+        for (int j=0;j<numQ;j++) {
+           string animalQ;cin >>animalQ;   
+           animalQuestions[i].push_back(animalQ);
+        }
+    }
+
+    int num_match =0;
+    int max_num_match =0;
+    
+    for (int i=0;i<num_entry;i++) {
+        for (int j=i+1;j<num_entry;j++) {
+        num_match=1; 
+        for (const auto& animalQ : animalQuestions[i]) {
+                 for (const auto& CompanimalQ : animalQuestions[j]) {
+                    if (animalQ == CompanimalQ ){   
+                        num_match ++;
+                        break;
+                    }
+                }    
+            } 
+            max_num_match = max(max_num_match,num_match);
+        }   
+    }
+   
+    cout << max_num_match << endl;
+
+}
